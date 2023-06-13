27ad267d916cac2cd421fe1f916afd53ea624a48
Pat Browne <pjbrowne011@gmail.com>
2023-06-12 22:59:04 -0400

    created initial dfa.c file

---
+++ dfa.c
@@ -0,0 +1,18 @@
+/* DFA State Machine Program
+
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void usage()
+{
+  const char *usage_str = "\
+Defining usage for the DFA simulator"
+
+int
+main(int argc, char *argv[])
+{
+  
+}
