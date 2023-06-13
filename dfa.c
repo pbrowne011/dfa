f0ee7d97bae1e1fa2285f69d63275b7dde7a26f0
Pat Browne <pjbrowne011@gmail.com>
2023-06-13 15:51:00 -0400

    added getopt for cl arguments

--- dfa.c
+++ dfa.c
@@ -2,6 +2,7 @@
 
  */
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,5 +15,30 @@ Defining usage for the DFA simulator"
 int
 main(int argc, char *argv[])
 {
-  
+  int c = 0;
+  int vflag = 0;
+  char dfa_file[512];
+  strcpy(dfa_file, "");
+
+  while((c = getopt(argc, argv, "d:hv")) != -1) {
+    switch(c)
+      {
+      case 'd':
+	strcpy(dfa_file, optarg);
+	break;
+      case 'h':
+	usage();
+	break;
+      case 'v':
+	vflag = 1;
+	break;
+      case '?':
+	usage();
+	break;
+      default:
+	usage();
+	break;
+      }
+
+  }
 }
